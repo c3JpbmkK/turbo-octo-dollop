7edd4f367bbb706b9962df93d3ba6a73e059b577
Srinivasan Mohana Arunachalam <s.mohanaarunachalam@reply.de>
2023-02-14 23:33:30 +0100

    Add more files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+## Utilities
+function writeError {
+    echo "Something went wrong"
+}
+
 ## Create kind cluster
-kind create cluster || echo "Something went wrong"
+kind create cluster || writeError
 
 ## Install argocd
 helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install --create-namespace --namespace argocd argo-cd argo/argo-cd
 
-ARGOCD_INITIAL_ADMIN_PASSWORD=$(kubectl  -n argocd get secrets argocd-initial-admin-secret  -o json | jq -r '.data.password' | base64 -d)
+## Login into the argo-cd server
+export ARGOCD_OPTS='--port-forward --port-forward-namespace argocd'
+while [ -z "$ARGOCD_INITIAL_ADMIN_SECRET" ]
+do
+ARGOCD_INITIAL_ADMIN_SECRET=$(kubectl -n argocd get secrets argocd-initial-admin-secret  -o json | jq -r '.data.password' | base64 -d || writeError)
+sleep 1
+done
+argocd login --username admin --password "$ARGOCD_INITIAL_ADMIN_SECRET" 2>/dev/null || writeError
+
+## Add cluster to local argocd intance
+argocd cluster list 2>/dev/null || writeError
+echo "Using context mega-featsrini-kubernetes-admin"
+argocd cluster add --yes --kubeconfig /home/srini/.kube/config mega-featsrini-kubernetes-admin 
+
+## Customize local argocd instance
+argocd proj create
--- teardown.sh
+++ teardown.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-## Delete argoproj CRDs
-kubectl  delete crds -l app.kubernetes.io/part-of=argocd || echo "Something went wrong"
+## Utilities
+function writeError {
+    echo "Something went wrong"
+}
 
+## Delete argoproj CRDs
+kubectl  delete crds -l app.kubernetes.io/part-of=argocd || writeError
 ## Delete kind cluster
-kind delete cluster || echo "Something went wrong"
+kind delete cluster || writeError
