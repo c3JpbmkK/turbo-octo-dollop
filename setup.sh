0582f6f0d06152cea045e617d69c91cd73b4d2df
Srinivasan Mohana Arunachalam <s.mohanaarunachalam@reply.de>
2023-02-17 13:06:02 +0100

    Adjust application destination

--- setup.sh
+++ setup.sh
@@ -18,9 +18,12 @@ helm upgrade --install --create-namespace --namespace argocd argo-cd argo/argo-c
 export ARGOCD_OPTS='--port-forward --port-forward-namespace argocd'
 while [ -z "$ARGOCD_INITIAL_ADMIN_SECRET" ]
 do
-ARGOCD_INITIAL_ADMIN_SECRET=$(kubectl -n argocd get secrets argocd-initial-admin-secret -o json 2>/dev/null | jq -r '.data.password' | base64 -d || writeError)
+ARGOCD_INITIAL_ADMIN_SECRET=$(kubectl -n argocd get secrets argocd-initial-admin-secret -o json 2>/dev/null \
+                            | jq -r '.data.password' \
+                            | base64 -d \
+                            || writeError)
 echo "Waiting for argocd"
-sleep 1
+sleep 10
 done
 argocd login --username admin --password "$ARGOCD_INITIAL_ADMIN_SECRET" 2>/dev/null || writeError
 
@@ -32,7 +35,7 @@ read -p "Add ${CURRENT_CONTEXT} to argocd clusters (y/n)? " confirm
 case $confirm in
     y|Y)
         echo "Adding cluster ${CURRENT_CONTEXT}"
-        argocd cluster add --yes --kubeconfig /home/"${HOST_USER}"/.kube/config ${CURRENT_CONTEXT} 2>/dev/null
+        argocd cluster add --yes --kubeconfig /home/"${HOST_USER}"/.kube/config --name "${CURRENT_CONTEXT//-admin}" "${CURRENT_CONTEXT}" 2>/dev/null
         ;;
     n|N)
         echo "Skipping cluster ${CURRENT_CONTEXT}"
