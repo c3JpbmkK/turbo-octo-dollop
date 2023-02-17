9250d873426d7bde527b0aab00b6b0cea26a7a20
Srinivasan Mohana Arunachalam <s.mohanaarunachalam@reply.de>
2023-02-17 12:45:09 +0100

    Add new files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+HOST_USER=srini
+
 ## Utilities
 function writeError {
     echo "Something went wrong"
@@ -16,20 +18,21 @@ helm upgrade --install --create-namespace --namespace argocd argo-cd argo/argo-c
 export ARGOCD_OPTS='--port-forward --port-forward-namespace argocd'
 while [ -z "$ARGOCD_INITIAL_ADMIN_SECRET" ]
 do
-ARGOCD_INITIAL_ADMIN_SECRET=$(kubectl -n argocd get secrets argocd-initial-admin-secret  -o json | jq -r '.data.password' | base64 -d || writeError)
+ARGOCD_INITIAL_ADMIN_SECRET=$(kubectl -n argocd get secrets argocd-initial-admin-secret -o json 2>/dev/null | jq -r '.data.password' | base64 -d || writeError)
+echo "Waiting for argocd"
 sleep 1
 done
 argocd login --username admin --password "$ARGOCD_INITIAL_ADMIN_SECRET" 2>/dev/null || writeError
 
 ## Add cluster to local argocd intance
 argocd cluster list 2>/dev/null || writeError
-CURRENT_CONTEXT=$(kubectl config current-context)
+CURRENT_CONTEXT=$(kubectl --kubeconfig /home/"${HOST_USER}"/.kube/config config current-context)
 echo "Current context is ${CURRENT_CONTEXT}"
 read -p "Add ${CURRENT_CONTEXT} to argocd clusters (y/n)? " confirm
 case $confirm in
     y|Y)
         echo "Adding cluster ${CURRENT_CONTEXT}"
-        argocd cluster add --yes --kubeconfig /home/srini/.kube/config ${CURRENT_CONTEXT}
+        argocd cluster add --yes --kubeconfig /home/"${HOST_USER}"/.kube/config ${CURRENT_CONTEXT} 2>/dev/null
         ;;
     n|N)
         echo "Skipping cluster ${CURRENT_CONTEXT}"
